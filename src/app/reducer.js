a2dea6718ca0c6bb2576c470b3f9f48d86f8fe54
sergTesst <sergiycheck@gmail.com>
2021-08-30 14:01:34 +0300

    implementation of rootReducer by hand

--- src/app/reducer.js
+++ src/app/reducer.js
@@ -1,16 +1,6 @@
-import { actionTypes } from '../features/appActionTypes'
+import todosReducer from "../features/todos/todoSlice";
+import filtersReducer from '../features/filters/filtersSlice';
 
-const initialState = {
-  todos: [
-    { id: 0, text: 'Learn React', completed: true },
-    { id: 1, text: 'Learn Redux', completed: false, color: 'purple' },
-    { id: 2, text: 'Build something fun!', completed: false, color: 'blue' },
-  ],
-  filters: {
-    status: 'All',
-    colors: [],
-  },
-}
 
 //reducer rules
 //	to calculate the new state based on the state and action arguments
@@ -33,52 +23,16 @@ const initialState = {
 	writing immutable updates by hand this way looks hard to remember
 */
 
-export default function appReducer(state = initialState, action) {
-  switch (action.type) {
-    case actionTypes.todosAdded: {
-      // returns new state object
-      return {
-        //that has all the existing state data
-        ...state,
-        //but has a new array for the `todos` field
-        todos: [
-          //with all of the old todos
-          ...state.todos,
-          //and the new todo object
-          {
-            id: nextTodoId(state.todos),
-            text: action.payload,
-            completed: false,
-          },
-        ],
-      }
-    }
-    case actionTypes.todosToggled: {
-      return {
-        ...state,
-        todos: state.todos.map((todo) => {
-          //if this isn't the todo item we're looking for, leave it alone
-          if (todo.id !== action.payload) {
-            return todo
-          }
-          //We've found the todo that has to change. Return a copy:
-          return {
-            ...todo,
-            completed: !todo.completed,
-          }
-        }),
-      }
-    }
-    case actionTypes.statusFilterChanged: {
-      return {
-        ...state,
-        filters: {
-          ...state.filters,
-          status: action.payload,
-        },
-      }
-    }
-    default:
-      return state
+
+// state = {
+//  todos:{...},
+//  filters:{...}
+// }
+ 
+export default function rootReducer(state = {}, action) {
+  //always return a new object for the root state
+  return {
+    todos:todosReducer(state.todos, action),
+    filters: filtersReducer(state.filters, action)
   }
 }
