71439e5205cae219675f31ab97c21aee8f33fe07
sergTesst <sergiycheck@gmail.com>
2021-08-31 19:24:27 +0300

    fix bug with re-redering on toggle todo with shallowEqual

--- src/features/todos/TodoList.js
+++ src/features/todos/TodoList.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import TodoListItem from './TodoListItem'
-import { useSelector } from 'react-redux'
+import { useSelector, shallowEqual} from 'react-redux'
 
 //returning new array references in selectors causes components to re-render every time
 
@@ -9,7 +9,7 @@ const selectTodoIds = state => state.todos.map(todo=>todo.id);
 const TodoList = () => {
 
 //useSelector can take a comparison func as a second arg
-  const todoIds = useSelector(selectTodoIds);
+  const todoIds = useSelector(selectTodoIds, shallowEqual);
 
   const renderedListItems = todoIds.map((todoId) => {
     return (
